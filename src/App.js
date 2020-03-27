11a9134ddd8e42fa74eda6f47d4c33e2fe1335a4
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-03-28 03:09:57 +0530

    about me section content added// education section content also added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Skills from './components/skills.js';
 import Projects from './components/projects.js';
 import Contact_me from './components/contact-me.js';
 import 'bootstrap/dist/css/bootstrap.css';
+import './components/stylesheets/reset.css';
+import './fonts/fontface.css';
 import './App.css';
 
 export default class App extends Component{
@@ -18,62 +20,104 @@ export default class App extends Component{
     this.handleClick4 = this.handleClick4.bind(this)
     this.handleClick5 = this.handleClick5.bind(this)
     this.state={
-      initial: 0
+      initial: 0,
+      l0: "#98003F",
+      l1: "#5E0047",
+      l2: "#5E0047",
+      l3: "#5E0047",
+      l4: "#5E0047",
+      l5: "#5E0047",
     }
   }
   handleClick(){
     this.setState({
-      initial:0
+      initial:0,
+      l0: "#98003F",
+      l1: "#5E0047",
+      l2: "#5E0047",
+      l3: "#5E0047",
+      l4: "#5E0047",
+      l5: "#5E0047",
     })
   }
   handleClick1(){
     this.setState({
-      initial:1
+      initial:1,
+      l0: "#5E0047",
+      l1: "#98003F",
+      l2: "#5E0047",
+      l3: "#5E0047",
+      l4: "#5E0047",
+      l5: "#5E0047",
     })
   }  handleClick2(){
     this.setState({
-      initial:2
+      initial:2,
+      l0: "#5E0047",
+      l1: "#5E0047",
+      l2: "#98003F",
+      l3: "#5E0047",
+      l4: "#5E0047",
+      l5: "#5E0047",
     })
   }  handleClick3(){
     this.setState({
-      initial:3
+      initial:3,
+      l0: "#5E0047",
+      l1: "#5E0047",
+      l2: "#5E0047",
+      l3: "#98003F",
+      l4: "#5E0047",
+      l5: "#5E0047",
     })
   }  handleClick4(){
     this.setState({
-      initial:4
+      initial:4,
+      l0: "#5E0047",
+      l1: "#5E0047",
+      l2: "#5E0047",
+      l3: "#5E0047",
+      l4: "#98003F",
+      l5: "#5E0047",
     })
   }  handleClick5(){
     this.setState({
-      initial:5
+      initial:5,
+      l0: "#5E0047",
+      l1: "#5E0047",
+      l2: "#5E0047",
+      l3: "#5E0047",
+      l4: "#5E0047",
+      l5: "#98003F",
     })
   }
   renderElement(){
-    if(this.state.initial == 0){
+    if(this.state.initial === 0){
       return(
         <About_me />
       )
     }
-    if(this.state.initial == 1){
+    if(this.state.initial === 1){
       return(
         <Experience />
       )
     }
-    if(this.state.initial == 2){
+    if(this.state.initial === 2){
       return(
         <Education />
       )
     }
-    if(this.state.initial == 3){
+    if(this.state.initial === 3){
       return(
         <Skills />
       )
     }
-    if(this.state.initial == 4){
+    if(this.state.initial === 4){
       return(
         <Projects />
       )
     }
-    if(this.state.initial == 5){
+    if(this.state.initial === 5){
       return(
         <Contact_me />
       )
@@ -81,16 +125,16 @@ export default class App extends Component{
   }
   render(){
     return(
-      <div className="row page-wrapper">
-        <div className="nav-list col-2 my-5">
-          <div onClick={this.handleClick} className="single-nav my-5 pl-3">About Me</div>
-          <div onClick={this.handleClick1} className="single-nav my-5 pl-3">Experience</div>  
-          <div onClick={this.handleClick2} className="single-nav my-5 pl-3">Education</div>  
-          <div onClick={this.handleClick3} className="single-nav my-5 pl-3">Skills</div>
-          <div onClick={this.handleClick4} className="single-nav my-5 pl-3">Projects</div>  
-          <div onClick={this.handleClick5} className="single-nav my-5 pl-3">Contact Me</div>  
+      <div className="page-wrapper d-flex">
+        <div className="nav-list col-2 px-0 my-5">
+          <div onClick={this.handleClick} style={{backgroundColor: this.state.l0}} className="single-nav my-5 pl-3 d-flex align-items-center">About Me</div>
+          <div onClick={this.handleClick1} style={{backgroundColor: this.state.l1}} className="single-nav my-5 pl-3 d-flex align-items-center">Experience</div>  
+          <div onClick={this.handleClick2} style={{backgroundColor: this.state.l2}} className="single-nav my-5 pl-3 d-flex align-items-center">Education</div>  
+          <div onClick={this.handleClick3} style={{backgroundColor: this.state.l3}} className="single-nav my-5 pl-3 d-flex align-items-center">Skills</div>
+          <div onClick={this.handleClick4} style={{backgroundColor: this.state.l4}} className="single-nav my-5 pl-3 d-flex align-items-center">Projects</div>  
+          <div onClick={this.handleClick5} style={{backgroundColor: this.state.l5}} className="single-nav my-5 pl-3 d-flex align-items-center">Contact Me</div>  
         </div>
-        <div className="display col-10">
+        <div className="display col-10 p-4">
           {this.renderElement()}
         </div>
       </div>
--- src/components/education.js
+++ src/components/education.js
@@ -1,7 +1,76 @@
-import React,{Component} from 'react'
+import React,{Component} from 'react';
+import './stylesheets/education.css';
 
 export default function Education(){
     return(
-        <div>hi</div>
+        <div class="container">
+
+            <div class="col-12 single-ed p-3 mb-4">
+                <div class="top d-flex justify-content-between">
+                    <div class="top-left d-flex flex-column">
+                        <h2>Pandit DeenDayal Petroleum University</h2>
+                        <p>Bachelor of Technology in Industrial Engineering</p>
+                    </div>
+                    <div class="time-span">
+                        <h2>
+                        Aug 2016- May 2020
+                        </h2>
+                    </div>
+                </div>
+                <div class="content">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur 
+                    in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla 
+                    commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. 
+                    Morbi dapibus porta quam laoreet placerat. 
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-12 single-ed p-3 mb-4">
+                <div class="top d-flex justify-content-between">
+                    <div class="top-left d-flex flex-column">
+                        <h2>Pandit DeenDayal Petroleum University</h2>
+                        <p>Bachelor of Technology in Industrial Engineering</p>
+                    </div>
+                    <div class="time-span">
+                        <h2>
+                        Aug 2016- May 2020
+                        </h2>
+                    </div>
+                </div>
+                <div class="content">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur 
+                    in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla 
+                    commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. 
+                    Morbi dapibus porta quam laoreet placerat. 
+                    </p>
+                </div>
+            </div>
+            
+            <div class="col-12 single-ed p-3 mb-4">
+                <div class="top d-flex justify-content-between">
+                    <div class="top-left d-flex flex-column">
+                        <h2>Pandit DeenDayal Petroleum University</h2>
+                        <p>Bachelor of Technology in Industrial Engineering</p>
+                    </div>
+                    <div class="time-span">
+                        <h2>
+                        Aug 2016- May 2020
+                        </h2>
+                    </div>
+                </div>
+                <div class="content">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur 
+                    in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla 
+                    commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. 
+                    Morbi dapibus porta quam laoreet placerat. 
+                    </p>
+                </div>
+            </div>
+
+        </div>
     )
 }

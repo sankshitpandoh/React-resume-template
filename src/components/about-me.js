0242739198fecc89d6002d25bedd8527a948900a
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-03-29 01:47:28 +0530

    education section added

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ReactWOW from 'react-wow'
 import '../fonts/fontface.css'
 import './stylesheets/reset.css';
 import './stylesheets/about-me.css';
@@ -8,27 +8,37 @@ export default function About_me(){
     return(
         <div class="container">
             <div class="about-me">
+
                 <div class="col-7 about-me-intro">
+
+                    <ReactWOW animation ='rotateInUpRight'>
                     <h2>
-                        Hey! I'm Sankshit Pandoh
+                        Hey! I'm <span>Sankshit Pandoh</span>
                     </h2>
+                    </ReactWOW>
+
+                    <ReactWOW animation ='rotateInUpRight' delay="0.5s">
                     <p class="text-justify">
                     Web Developer from Jammu, India.
-                    Currently employed by Uplers where I work as a Front-end developer. 
-                    I have rich experience in developing Front-end systems and writing apllication code. 
+                    Currently employed by Uplers where I work as a <span>Front-end developer</span>.
+                    I have rich experience in developing <span> Front-end systems and writing apllication code</span>. 
                     Working with command lines and writing modules interest me. I aspire to become a full
-                    stack developer in near future. I am a big Sci-Fi junkie and a firm believer of Quantum immortality
-                    and Simulation theory.  
+                    stack developer in near future. I am a big Sci-Fi junkie and a firm believer of <span> Quantum immortality
+                    and Simulation theory</span>.  
                     </p>
+                    </ReactWOW>
+
                 </div>
-                <div class="col-5 d-flex">
-                    <ul class="d-flex flex-column">
-                        <li class="mb-4"><strong>Age:</strong><span class="ml-3">21</span></li>
-                        <li class="mb-4"><strong>Freelance:</strong><span class="ml-3">Available</span></li>  
-                        <li class="mb-4"><strong>Home Town:</strong><span class="ml-3">Jammu, India</span></li>
-                        <li class="mb-4"><strong>Current Address:</strong><span class="ml-3">Ahmedabad, Gujarat</span></li>
-                    </ul>
-                </div>
+
+                    <div class="col-5 d-flex">
+                        <ul class="d-flex flex-column">
+                            <li class="mb-4"><strong>Age:</strong><span class="ml-3">21</span></li>
+                            <li class="mb-4"><strong>Freelance:</strong><span class="ml-3">Available</span></li>  
+                            <li class="mb-4"><strong>Home Town:</strong><span class="ml-3">Jammu, India</span></li>
+                            <li class="mb-4"><strong>Current Address:</strong><span class="ml-3">Ahmedabad, Gujarat</span></li>
+                        </ul>
+                    </div>
+
             </div>
         </div>
     )
--- src/components/education.js
+++ src/components/education.js
@@ -1,79 +1,74 @@
 import React,{Component} from 'react';
+import './stylesheets/reset.css';
 import './stylesheets/education.css';
 import ReactWOW from 'react-wow';
 
 export default function Education(){
     return(
-        <ReactWOW animation ='bounceInLeft'>
-        <div class="container">
-
-            <div class="col-12 single-ed p-3 mb-5">
-                <div class="top d-flex justify-content-between">
-                    <div class="top-left d-flex flex-column">
+        <ReactWOW animation ='bounceInUp' delay="0.5s">
+        <div class="container row">
+            <div class="col-10">
+                <div class = "single-ed p-3 mb-3">
+                    <div class="top d-flex">
                         <h2>Pandit DeenDayal Petroleum University</h2>
-                        <p>Bachelor of Technology in Industrial Engineering</p>
+                        <span class="time ml-auto">2016 - 2020</span>
                     </div>
-                    <div class="time-span">
-                        <h2>
-                            2016 - 2020
-                        </h2>
+                    <div class="content">
+                        <p class="mb-1">
+                            Bachelor of Technology
+                        </p>
+                        <p class="mb-1">
+                            Department of Industrial Engineering.
+                        </p>
+                        <p class="mb-1">
+                            CPI:<span> 7.2
+</span>                        </p>
                     </div>
-                </div>
-                <div class="content">
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur 
-                    in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla 
-                    commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. 
-                    Morbi dapibus porta quam laoreet placerat. 
-                    </p>
                 </div>
             </div>
 
-            <div class="col-12 single-ed p-3 mb-5">
-                <div class="top d-flex justify-content-between">
-                    <div class="top-left d-flex flex-column">
+            <div class="col-8">
+                <div class = "single-ed p-3 mb-3">
+                    <div class="top d-flex">
                         <h2>Ranbir Higher Secondary School</h2>
-                        <p>Junior College</p>
+                        <span class="time ml-auto">2014 - 2016</span>
                     </div>
-                    <div class="time-span">
-                        <h2>
-                            2014 - 2016
-                        </h2>
+                    <div class="content">
+                        <p class="mb-1">
+                            Junior College
+                        </p>
+                        <p class="mb-1">
+                            PCM with Computer Science.
+                        </p>
+                        <p class="mb-1">
+                            CPI:<span> 8.64</span>
+                        </p>
                     </div>
-                </div>
-                <div class="content">
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur 
-                    in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla 
-                    commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. 
-                    Morbi dapibus porta quam laoreet placerat. 
-                    </p>
                 </div>
             </div>
-            
-            <div class="col-12 single-ed p-3 mb-5">
-                <div class="top d-flex justify-content-between">
-                    <div class="top-left d-flex flex-column">
+
+            <div class="col-6">
+
+                <div class = "single-ed p-3 mb-3">
+                    <div class="top d-flex align-items-center">
                         <h2>St. Peter's High School</h2>
-                        <p>High School</p>
+                        <span class="time ml-auto">2004 - 2014</span>
                     </div>
-                    <div class="time-span">
-                        <h2>
-                            2004 - 2014
-                        </h2>
+                    <div class="content">
+                        <p class="mb-1">
+                            High School
+                        </p>
+                        <p class="mb-1">
+                            English, Social Studies, PCM with Computer Science.
+                        </p>
+                        <p class="mb-1">
+                            CPI:<span> 9.36</span>
+                        </p>
                     </div>
-                </div>
-                <div class="content">
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur 
-                    in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla 
-                    commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. 
-                    Morbi dapibus porta quam laoreet placerat. 
-                    </p>
                 </div>
             </div>
 
         </div>
-        </ReactWOW> 
+        </ReactWOW>
     )
 }

890c6277716323310ab0c9cab1b406bd49c51fc0
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-03-31 01:13:27 +0530

    form method added in contact me

--- src/components/contact-me.js
+++ src/components/contact-me.js
@@ -1,25 +1,33 @@
-import React,{Component} from 'react'
+import React,{Component} from 'react';
+import '../fonts/fontface.css'
+import './stylesheets/reset.css';
+import './stylesheets/contact-me.css';
 
 export default function Contact_me(){
     return(
         <div class="container">
-            <div class="col-12">
-                <h3>Please be nice to me. Thanks</h3>
-                <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+            <div class="row">
+                <div class="col-7 lead">
+                    <p class="text-justify mb-4">
+                        Have a project for me? Think I'd be a good fit for your team ?
+                         <span> I'd love to hear from you, </span> give me a shout by using
+                        the form below.
+                    </p>
+                    <form name="contact" method="post">
+                        <input type="hidden" name="form-name" value="contact" required/>
+
+
+                        <input class="col-12 mb-3" placeholder="Name" type="text" name="name"/>
+
+                        <input class="col-12 mb-3" placeholder="Email" type="email" name="email"/>
+                        <input class="col-12 mb-3" placeholder="Subject" type="text" name="subject"/>
+
+                        <textarea class="col-12 mb-3" placeholder="Message" name="message"/>
+                        
+                        <button class="submit-btn" type="submit">Send</button>
+                        
+                     </form>
+                </div>
             </div>
         </div>
     )

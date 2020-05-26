9c679755213ab69dff93472e21b9991d3d02d06c
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-05-26 18:42:57 +0530

    skill section structure improved and made easy to maintain in future

---
+++ src/components/singleSkill.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SingleSkill(props){
+    const skills = props.skillList.map((x, index) => {
+        return <div className="col-lg-3 col-6 col-md-6 my-5 single-skill">
+                    <h3 className="mb-2">{props.skillList[index].skillName}</h3>
+                    <div className="progress">
+                        <div className="progress-bar" style={{width: `${props.skillList[index].skillLevel}`}}></div>
+                    </div>
+                </div>
+    })
+    return(
+        <div className="row">
+        {skills}
+        </div>
+    )
+}
+
+export default SingleSkill;
